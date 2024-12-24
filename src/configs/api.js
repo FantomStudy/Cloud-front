f4bad4d8f55bf70edbd25cb787ab43a10e82742b
FantomStudy <144556354+FantomStudy@users.noreply.github.com>
2024-12-25 04:32:00 +0500

    Update api.js

--- src/configs/api.js
+++ src/configs/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // настройка аксиос
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://disk-api-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
